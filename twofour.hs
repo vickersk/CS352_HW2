77083bbf7e768a32dedeb80f9fe478ee09506b86
vickersk <kaiv14@comcast.com>
2021-09-15 23:53:34 -0700

    Complete Question 1
    Implemented the data type for a 2-4 tree in Haskell using a data decleration
    for algebraic types, where the type is named TwoFourTree. The TwoFourTree type
    includes all four node types (Nil, TwoNode, ThreeNode and FourNode) that have
    leaves internally defined as TwoFourTrees. TwoFourTree is also deriving Show.
    
    Added the sample TwoFourTree variable (sampleTree) with four non-nil tree nodes
    and eight data elements.

--- twofour.hs
+++ twofour.hs
@@ -4,3 +4,10 @@ module TwoFour (
 ) where
 
 -- MODIFY BELOW THIS LINE
+data TwoFourTree p = Nil 
+                    | TwoNode p (TwoFourTree p) (TwoFourTree p) 
+                    | ThreeNode p p (TwoFourTree p) (TwoFourTree p) (TwoFourTree p)
+                    | FourNode p p p (TwoFourTree p) (TwoFourTree p) (TwoFourTree p) (TwoFourTree p)
+    deriving (Show)
+
+sampleTree =  ThreeNode 2 7 (TwoNode 0 (Nil) (Nil)) (ThreeNode 3 5 (Nil) (Nil) (Nil)) (FourNode 6 10 13 (Nil) (Nil) (Nil) (Nil))
