5c6152153fca562f8399ba1d1cef1aa9df00e9e3
vickersk <kaiv14@comcast.com>
2021-10-02 19:04:16 -0700

    Complete Question 2
    Implemented the typeclass Sized t that has one function, size, that is
    type t -> Int, which returns the size of an object. Implemented Sized
    for [a] where the size of the list is its length. Implemented Sized for
    TwoFourTree a, where the size of the 2-4 tree is the number of data
    elements it contains.

--- sized.hs
+++ sized.hs
@@ -3,3 +3,15 @@ module Sized where
 import TwoFour
 
 -- MODIFY BELOW THIS LINE
+
+class Sized t where
+    size :: t -> Int
+
+instance Sized [a] where
+    size list = length(list)
+
+instance Sized (TwoFourTree a) where
+    size Nil = 0
+    size (TwoNode _ tree1 tree2) = 1 + size tree1 + size tree2
+    size (ThreeNode _ _ tree1 tree2 tree3) = 2 + size tree1 + size tree2 + size tree3
+    size (FourNode _ _ _ tree1 tree2 tree3 tree4) = 3 + size tree1 + size tree2 + size tree3 + size tree4
--- twofour.hs
+++ twofour.hs
@@ -4,10 +4,10 @@ module TwoFour (
 ) where
 
 -- MODIFY BELOW THIS LINE
-data TwoFourTree p = Nil 
-                    | TwoNode p (TwoFourTree p) (TwoFourTree p) 
-                    | ThreeNode p p (TwoFourTree p) (TwoFourTree p) (TwoFourTree p)
-                    | FourNode p p p (TwoFourTree p) (TwoFourTree p) (TwoFourTree p) (TwoFourTree p)
+data TwoFourTree a = Nil 
+                    | TwoNode a (TwoFourTree a) (TwoFourTree a) 
+                    | ThreeNode a a (TwoFourTree a) (TwoFourTree a) (TwoFourTree a)
+                    | FourNode a a a (TwoFourTree a) (TwoFourTree a) (TwoFourTree a) (TwoFourTree a)
     deriving (Show)
 
 sampleTree =  ThreeNode 2 7 (TwoNode 0 (Nil) (Nil)) (ThreeNode 3 5 (Nil) (Nil) (Nil)) (FourNode 6 10 13 (Nil) (Nil) (Nil) (Nil))
